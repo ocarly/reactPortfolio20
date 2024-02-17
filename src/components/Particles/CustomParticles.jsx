0a95ac9e1ff7873c2e89f88fb509b2eff7a50407
BootCamp_Xolo <141790916+ocarly@users.noreply.github.com>
2024-02-16 22:22:30 -0600

    attempting to display particles again.

--- src/components/Particles/CustomParticles.jsx
+++ src/components/Particles/CustomParticles.jsx
@@ -1,33 +1,12 @@
-
+import PropTypes from 'prop-types';
 import Particles from 'react-tsparticles';
 
 
-const MyComponent = () => {
-  const options = {
-    particles: {
-      number: {
-        value: 50, // Number of particles
-      },
-      size: {
-        value: 3, // Size of particles
-      },
-      color: {
-        value: "#ff0000", // Color of particles
-      },
-      shape: {
-        type: "circle", // Shape of particles
-      },
-      line_linked: {
-        enable: true, // Enable/Disable lines between particles
-        distance: 150, // Distance between particles to link
-        color: "#ffffff", // Color of lines
-        opacity: 0.4, // Opacity of lines
-        width: 1, // Width of lines
-      },
-    },
-  };
-
+const CustomParticles = ({ options }) => {
   return <Particles options={options} />;
 };
+CustomParticles.propTypes = {
+  options: PropTypes.object.isRequired,
+};
 
-export default MyComponent;
+export default CustomParticles;
